a480007ad02822bc599c9f6f081fff756a962392
vijayakarnati <vijayakarnati@gmail.com>
2020-03-06 13:54:03 -0600

    first commit

---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,19 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	@Test
+	public void BrowserAutomation() {
+
+		System.out.println("BrowserAutomation");
+	}
+
+	@Test
+	public void elementsUI() {
+
+		System.out.println("elementsUI");
+	}
+
+}
